f9c390d5f0933548d92ea709f9a9b455536713cf
Cristian-DP <cristian.pereyra@mi.unc.edu.ar>
2021-11-18 14:01:51 -0300

    test between interface and alu

---
+++ TrabajosPacticos/TP2/UART.srcs/sources_1/new/test_interface_Rx.v
@@ -0,0 +1,88 @@
+`timescale 1ns / 1ps
+
+
+module test_interface_Rx;
+    parameter           NB_DATA = 8;
+    parameter           PERIOD  = 20; // debe dar 50 Mh
+    parameter           [NB_DATA-1 : 0] da  = 8'b00000001;
+    parameter           [NB_DATA-1 : 0] db  = 8'b00000010;
+    parameter           [NB_DATA-1 : 0] dop = 8'b00100000;
+    parameter           [NB_DATA-1 : 0] dop1 = 8'b00100100;
+    
+    reg                 [NB_DATA-1 : 0] DATO;
+    wire                 [NB_DATA-1 : 0] o_data_A ;
+    wire                 [NB_DATA-1 : 0] o_data_B ;
+    wire                 [NB_DATA-1 : 0] o_data_Op;
+    wire                 [NB_DATA-1 : 0] o_alu;
+    reg                                 wr;
+    reg                                 CLOCK;
+    reg                  [4:0]          count;
+    
+    interface myinterface (.DATO(DATO), .wr(wr), .CLOCK(CLOCK),
+                            .o_data_A(o_data_A), .o_data_B(o_data_B),
+                            .o_data_Op(o_data_Op) , .o_alu(o_alu));
+                            
+    initial
+        begin
+            CLOCK   = 0;
+            wr      = 0;
+            count   = 1;
+        end
+        
+    always begin
+        #(PERIOD/2) CLOCK = ~CLOCK;
+        #(PERIOD/2);
+    end
+    
+    always @(posedge CLOCK)
+    begin
+        case (count)
+            4'b0001:
+            begin
+                count <= count + 1;
+            end
+            4'b0100:
+            begin
+                DATO    <= da;
+                wr      <= 1;
+                count   <= count +1;
+            end
+            4'b0101:
+            begin
+                wr      <= 0;
+                count   <= count +1;
+            end
+            4'b0110:
+            begin
+                DATO    <= db;
+                wr      <= 1;
+                count   <= count +1;
+            end
+            4'b0111:
+            begin
+                wr      <= 0;
+                count   <= count +1;
+            end
+            4'b1000:
+            begin
+                DATO    <= dop;
+                wr      <= 1;
+                count   <= count +1;
+            end
+            4'b1001:
+            begin
+                wr      <= 0;
+                count   <= count +1;
+            end
+            4'b1010:
+            begin
+                count   <= 1;
+            end
+            
+            default: count <= count +1;
+        endcase
+    
+    end
+    
+    
+endmodule
