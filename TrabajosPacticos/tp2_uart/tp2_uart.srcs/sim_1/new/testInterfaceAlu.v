fe12d713850655dee63e973d1c8d7670c6d9cc13
Cristian-DP <cristian.pereyra@mi.unc.edu.ar>
2021-11-23 14:28:28 -0300

    implementation of the state for tx

--- TrabajosPacticos/tp2_uart/tp2_uart.srcs/sim_1/new/testInterfaceAlu.v
+++ TrabajosPacticos/tp2_uart/tp2_uart.srcs/sim_1/new/testInterfaceAlu.v
@@ -4,35 +4,36 @@
 module testInterfaceAlu;
     parameter           NB_DATA = 8;
     parameter           PERIOD  = 20; // debe dar 50 Mh
-    parameter           [NB_DATA-1 : 0] da  = 8'b00000001;
+    parameter           [NB_DATA-1 : 0] da  = 8'b00000011;
     parameter           [NB_DATA-1 : 0] db  = 8'b00000010;
     parameter           [NB_DATA-1 : 0] dop = 8'b00100000;
     parameter           [NB_DATA-1 : 0] dop1 = 8'b00100100;
     
-    reg                 [NB_DATA-1 : 0] DATO;
+    reg                  [NB_DATA-1 : 0] DATO;
     wire                 [NB_DATA-1 : 0] o_data_A ;
     wire                 [NB_DATA-1 : 0] o_data_B ;
     wire                 [NB_DATA-1 : 0] o_data_Op;
     wire                 [NB_DATA-1 : 0] o_alu;
+    wire                 [NB_DATA-1 : 0] o_tx;
+    wire                                 empty;
     reg                                 wr;
     reg                                 CLOCK;
     reg                  [4:0]          count;
+    reg                                 flags;
     
-    interface_uart myinterface_uart (.DATO(DATO), .wr(wr), .CLOCK(CLOCK),
+    interface_uart myinterface_uart (.in_rx(DATO), .wr(wr), .CLOCK(CLOCK),
                             .o_data_A(o_data_A), .o_data_B(o_data_B),
-                            .o_data_Op(o_data_Op) , .o_alu(o_alu));
+                            .o_data_Op(o_data_Op) , .in_alu(o_alu), 
+                            .o_tx(o_tx), .empty(empty));
                             
     alu myAlu (.i_a(o_data_A), .i_b(o_data_B), .i_op(o_data_Op[5:0]), .o_o(o_alu));
     initial
         begin
             CLOCK   = 0;
             wr      = 0;
-            count   = 1;
-            DATO = 0;
-//            o_data_A = 0;
-//            o_data_B = 0;
-//            o_data_Op = 0;
-//            o_alu;
+            count   = 0;
+            DATO    = 8'b00000011;
+            flags   = 0;
         end
         
     always begin
@@ -43,48 +44,38 @@ module testInterfaceAlu;
     always @(posedge CLOCK)
     begin
         case (count)
-            4'b0001:
-            begin
-                count <= count + 1;
-            end
-            4'b0100:
+            2'b00:
             begin
                 DATO    <= da;
                 wr      <= 1;
                 count   <= count +1;
             end
-            4'b0101:
+            2'b01:
             begin
                 wr      <= 0;
-                count   <= count +1;
-            end
-            4'b0110:
-            begin
                 DATO    <= db;
                 wr      <= 1;
                 count   <= count +1;
             end
-            4'b0111:
+            2'b10:
             begin
                 wr      <= 0;
-                count   <= count +1;
-            end
-            4'b1000:
-            begin
-                DATO    <= dop;
+                if (flags) begin
+                    DATO    <= dop;
+                    flags   <= 0;
+                end
+                else begin
+                    DATO    <= dop1;
+                    flags   <= 1;
+                end
                 wr      <= 1;
                 count   <= count +1;
             end
-            4'b1001:
+            2'b11:
             begin
                 wr      <= 0;
-                count   <= count +1;
+                count   <= 0;
             end
-            4'b1010:
-            begin
-                count   <= 1;
-            end
-            
             default: count <= count +1;
         endcase
     
--- TrabajosPacticos/tp2_uart/tp2_uart.srcs/sources_1/new/interface_uart.v
+++ TrabajosPacticos/tp2_uart/tp2_uart.srcs/sources_1/new/interface_uart.v
@@ -4,43 +4,49 @@
 module interface_uart
 #(
     /** numero de bits - datos*/
-    parameter NB_DATA   = 8,
-    parameter NB_STATE  = 4
+    parameter NB_DATA   = 8,                        // cantidad de bits de la trama
+    parameter NB_STATE  = 4                         // catnidad de estados de la interface
 )
 (
     /* DATO */
-    input   wire                    wr,
-    input   wire    [NB_DATA - 1:0] DATO,
+    input   wire                    wr,             // habilitador para leer la entrada provista por rx uart
+    input   wire    [NB_DATA - 1:0] in_rx,          // Se presenta los bit provistos por rx uart
+    input   wire    [NB_DATA - 1:0] in_alu,         // Se presenta los bits provistos por la salida de la ALU (resultado de la alu)
     /* SALIDA*/
-    output  wire    [NB_DATA - 1:0] o_data_A,
-    output  wire    [NB_DATA - 1:0] o_data_B,
-    output  wire    [NB_DATA - 1:0] o_data_Op,
-    output  wire    [NB_DATA - 1:0] o_alu,
-    /* SELECT */
-//    input   wire selA,
-//    input   wire selB,
-//    input   wire selOp,
+    output  wire    [NB_DATA - 1:0] o_data_A,       // Se presenta el dato A a la alu
+    output  wire    [NB_DATA - 1:0] o_data_B,       // Se presenta el dato B a la Alu
+    output  wire    [NB_DATA - 1:0] o_data_Op,      // Se presenta el dato OP a la Alu
+    output  wire    [NB_DATA - 1:0] o_tx,           // Se presenta el resutlado a tx
+    output  wire                    empty,           // bit para avisar a tx que puede leer
     /* CLOCK */
-    input wire CLOCK
+    input wire CLOCK                                // clock que alimenta el sistema
 );
 
-  reg    [NB_DATA - 1:0] DA_reg;
-  reg    [NB_DATA - 1:0] DB_reg;
-  reg    [5          :0] OP_reg;
-  reg    [2:0]           count_data;
+  reg    [NB_DATA - 1:0] DA_reg      = 0;            // registro del dato A
+  reg    [NB_DATA - 1:0] DB_reg      = 0;            // registro del dato B
+  reg    [5          :0] OP_reg      = 0;            // registro del dato OP
+  reg    [NB_DATA - 1:0] in_alu_reg  = 0;           // registro del dato provisto por la salidad de la alu
+  reg    [2:0]           count_data  = 0;            // registro del contador de datos
+  reg                    read_alu    = 0;            // registro del contador de datos
+  reg                    empty_reg   = 1;            // registro de aviso para que rx lea el dato 
   // estados de la fsm
   localparam [NB_STATE-1 :0]
         STATE_DATA_A    = 4'b00001,
         STATE_DATA_B    = 4'b00010,
         STATE_DATA_OP   = 4'b00100,
-        STATE_TX   = 4'b01000;
+        STATE_TX        = 4'b01000;
 
     reg [NB_STATE - 1:0] current_state  = STATE_DATA_A;
     reg [NB_STATE - 1:0] next_state     = STATE_DATA_A;
    
    
    always @(posedge CLOCK) 
+   begin
         current_state   <= next_state;
+        if (empty_reg == 0) empty_reg = 1;
+        else  empty_reg = 0;
+        
+   end
    
    always @(*)
    begin
@@ -48,8 +54,7 @@ module interface_uart
             STATE_DATA_A:
                 begin
                     if(wr) begin
-                        DA_reg = DATO;
-                        count_data = 1;
+                        DA_reg = in_rx;
                         next_state = STATE_DATA_B;
                     end 
                     else begin
@@ -60,8 +65,7 @@ module interface_uart
             STATE_DATA_B:
             begin
                 if(wr) begin
-                        DB_reg = DATO;
-                        count_data = 2;
+                        DB_reg = in_rx;
                         next_state = STATE_DATA_OP;
                     end 
                     else begin
@@ -72,9 +76,9 @@ module interface_uart
             STATE_DATA_OP:
             begin
                 if(wr) begin
-                        OP_reg = DATO;
-                        count_data = 3;
+                        OP_reg = in_rx[5:0];
                         next_state = STATE_TX;
+                        in_alu_reg  = in_alu;
                     end 
                     else begin
                         next_state = STATE_DATA_OP;
@@ -83,10 +87,10 @@ module interface_uart
             // -------------------------------------------------------------------------- //
             STATE_TX:
             begin
-                next_state = STATE_DATA_A;
-                count_data = 0;
+                empty       = 0;
+                next_state  = STATE_DATA_A;
             end  
-              
+            
         endcase
    
    end
@@ -94,4 +98,6 @@ module interface_uart
     assign o_data_A     = DA_reg;
     assign o_data_B     = DB_reg;
     assign o_data_Op    = OP_reg;
+    assign o_tx         = in_alu_reg;
+    assign empty        = empty_reg;
 endmodule
